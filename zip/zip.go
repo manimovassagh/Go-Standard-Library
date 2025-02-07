cc8e103e2260f14e14921ba921fa12078f5ad313
Mani Movassagh <mani.mgh@gmail.com>
2025-02-08 00:42:46 +0100

    Move test data to zip directory and update references

--- zip/zip.go
+++ zip/zip.go
@@ -10,7 +10,7 @@ import (
 
 func Zipper() {
 	// Open a zip archive for reading.
-	r, err := zip.OpenReader("testdata/readme.zip")
+	r, err := zip.OpenReader("zip/testdata/readme.zip")
 	if err != nil {
 		log.Fatal(err)
 	}
