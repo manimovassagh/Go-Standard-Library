0156da4759917c9b8cc6c23c193367c8f34838fd
Mani Movassagh <mani.mgh@gmail.com>
2025-02-08 20:50:18 +0100

    Comment out comparison function call in main

--- main.go
+++ main.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	comparing "github.com/manimovassagh/go-standard-library/coparing"
 	ziping "github.com/manimovassagh/go-standard-library/zip"
 )
 
 func main() {
 
 	ziping.Zipper()
-	comparing.TryComp()
+	//comparing.TryComp()
 }
